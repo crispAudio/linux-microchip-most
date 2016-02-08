8b9e86aa5141bdfbaa22c735ea71c2ac897eb76e
Christian Gromm <christian.gromm@microchip.com>
2016-02-08 14:56:11 +0100

    hdm-usb: synchronize workqueue function wq_clear_halt with usb_disconnect

--- drivers/staging/most/hdm-usb/hdm_usb.c
+++ drivers/staging/most/hdm-usb/hdm_usb.c
@@ -932,12 +932,14 @@ static void wq_clear_halt(struct work_struct *wq_obj)
 	pipe = urb->pipe;
 	dev = urb->dev;
 
+	mutex_lock(&mdev->io_mutex);
 	free_anchored_buffers(mdev, channel, MBO_E_INVAL);
 	if (usb_clear_halt(dev, pipe))
 		dev_warn(&mdev->usb_device->dev, "Failed to reset endpoint.\n");
 
 	if (mdev->conf[channel].direction & MOST_CH_TX)
 		most_resume_enqueue(&mdev->iface, channel);
+	mutex_unlock(&mdev->io_mutex);
 }
 
 /**
