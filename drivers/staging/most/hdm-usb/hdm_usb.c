beb252863007f27d2f2b12a252b148130caa7391
Christian Gromm <christian.gromm@microchip.com>
2017-12-05 15:24:42 +0100

    staging: most: usb: add ep number to log
    This patch adds the endpoint number of the USB pipe that reports to be
    broken into the log message. It is needed to make debugging for
    applications more comfortable.
    
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>

--- drivers/staging/most/hdm-usb/hdm_usb.c
+++ drivers/staging/most/hdm-usb/hdm_usb.c
@@ -362,7 +362,8 @@ static void hdm_write_completion(struct urb *urb)
 			mbo->status = MBO_SUCCESS;
 			break;
 		case -EPIPE:
-			dev_warn(dev, "Broken OUT pipe detected\n");
+			dev_warn(dev, "Broken pipe on ep%02x\n",
+				 mdev->ep_address[channel]);
 			mdev->is_channel_healthy[channel] = false;
 			mdev->clear_work[channel].pipe = urb->pipe;
 			schedule_work(&mdev->clear_work[channel].ws);
@@ -515,7 +516,8 @@ static void hdm_read_completion(struct urb *urb)
 			}
 			break;
 		case -EPIPE:
-			dev_warn(dev, "Broken IN pipe detected\n");
+			dev_warn(dev, "Broken pipe on ep%02x\n",
+				 mdev->ep_address[channel]);
 			mdev->is_channel_healthy[channel] = false;
 			mdev->clear_work[channel].pipe = urb->pipe;
 			schedule_work(&mdev->clear_work[channel].ws);
@@ -525,7 +527,8 @@ static void hdm_read_completion(struct urb *urb)
 			mbo->status = MBO_E_CLOSE;
 			break;
 		case -EOVERFLOW:
-			dev_warn(dev, "Babble on IN pipe detected\n");
+			dev_warn(dev, "Babble on ep%02x\n",
+				 mdev->ep_address[channel]);
 			break;
 		}
 	}
